0348af25e983ceb088f46bb52b5df8bccc8a30a0
yeeun <yeeeun910@gmail.com>
2022-12-22 06:04:59 +0900

    layout and dialog

---
+++ lib/screens/habit/edit_dialog.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class EditDialog extends StatefulWidget {
+  // final String title, descriptions, text;
+  // final Image img;
+
+  const EditDialog({
+    super.key,
+  });
+
+  @override
+  _EditDialogState createState() => _EditDialogState();
+}
+
+class _EditDialogState extends State<EditDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      child: contentBox(context),
+    );
+  }
+
+  contentBox(context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: Theme.of(context).backgroundColor,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text("Title"),
+          Text("Day"),
+          Text("Info"),
+          Align(
+            alignment: Alignment.topRight,
+            child: IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: const Icon(Icons.close),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
