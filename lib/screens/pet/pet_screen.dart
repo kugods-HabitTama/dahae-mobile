7eb10f9a1ba3484e9f7200d767b0ac1e4a2d5ef5
yeeun <yeeeun910@gmail.com>
2022-12-22 16:21:56 +0900

    make pet dir

--- lib/screens/pet/pet_screen.dart
+++ lib/screens/pet/pet_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class PetScreen extends StatelessWidget {
+class PetScreen extends StatefulWidget {
   const PetScreen({super.key});
 
+  @override
+  State<PetScreen> createState() => _PetScreenState();
+}
+
+class _PetScreenState extends State<PetScreen> {
   @override
   Widget build(BuildContext context) {
     return const Center(child: Text('Tama'));
