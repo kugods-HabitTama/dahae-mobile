e1884a0b6e412b39f89af4c20abae8aac21aaf1b
SeongMin <2018320221@korea.ac.kr>
2023-01-04 23:22:45 +0900

    modified:   android/app/src/main/res/mipmap-hdpi/ic_launcher.png 	modified:   android/app/src/main/res/mipmap-mdpi/ic_launcher.png 	modified:   android/app/src/main/res/mipmap-xhdpi/ic_launcher.png 	modified:   android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png 	modified:   android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png 	new file:   assets/icons/calendar.png 	new file:   assets/icons/calendar_act.png 	new file:   assets/icons/pet.png 	new file:   assets/icons/pet_act.png 	new file:   assets/icons/profile.png 	new file:   assets/icons/profile_act.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/100.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/102.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/1024.png     new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/114.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/120.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/128.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/144.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/152.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/16.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/167.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/172.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/180.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/196.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/20.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/216.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/256.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/29.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/32.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/40.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/48.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/50.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/512.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/55.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/57.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/58.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/60.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/64.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/66.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/72.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/76.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/80.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/87.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/88.png 	new file:   ios/Runner/Assets.xcassets/AppIcon.appiconset/92.png 	modified:   ios/Runner/Assets.xcassets/AppIcon.appiconset/Contents.json 	modified:   lib/screens/home_scaffold.dart 	modified:   pubspec.yaml

--- lib/screens/home_scaffold.dart
+++ lib/screens/home_scaffold.dart
@@ -18,13 +18,32 @@ class HomeScaffold extends StatelessWidget {
   }
 }
 
-class NavBar extends StatelessWidget {
+class NavBar extends StatefulWidget {
   const NavBar({super.key});
 
   @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  int _menu = 0;
+
+  @override
   Widget build(BuildContext context) {
+    var calendar_icon = (_menu == 0)
+        ? Image.asset("assets/icons/calendar_act.png", width: 30, height: 30)
+        : Image.asset("assets/icons/calendar.png", width: 30, height: 30);
+
+    var pet_icon = (_menu == 1)
+        ? Image.asset("assets/icons/pet_act.png", width: 30, height: 30)
+        : Image.asset("assets/icons/pet.png", width: 30, height: 30);
+
+    var profile_icon = (_menu == 2)
+        ? Image.asset("assets/icons/profile_act.png", width: 30, height: 30)
+        : Image.asset("assets/icons/profile.png", width: 30, height: 30);
+
     return Container(
-      height: 60,
+      height: 80,
       decoration: BoxDecoration(
         borderRadius: const BorderRadius.vertical(top: Radius.circular(16.0)),
         boxShadow: [
@@ -40,18 +59,32 @@ class NavBar extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           IconButton(
-            onPressed: () => GoRouter.of(context).go('/habit'),
-            icon: const Icon(Icons.calendar_month),
+            onPressed: (() {
+              _menu = 0;
+              GoRouter.of(context).go('/habit');
+            }),
+            icon: calendar_icon,
+            /*icon:
+                Image.asset("assets/icons/calendar.png", width: 30, height: 30),
+            isSelected: (_menu == 0) ? true : false,
+            selectedIcon: Image.asset("assets/icons/calendar_act.png",
+                width: 30, height: 30),*/
             enableFeedback: false,
           ),
           IconButton(
-            onPressed: () => GoRouter.of(context).go('/pet'),
-            icon: const Icon(Icons.pets),
+            onPressed: (() {
+              _menu = 1;
+              GoRouter.of(context).go('/pet');
+            }),
+            icon: pet_icon,
             enableFeedback: false,
           ),
           IconButton(
-            onPressed: () => GoRouter.of(context).go('/profile'),
-            icon: const Icon(Icons.person),
+            onPressed: (() {
+              _menu = 2;
+              GoRouter.of(context).go('/profile');
+            }),
+            icon: profile_icon,
             enableFeedback: false,
           ),
         ],
