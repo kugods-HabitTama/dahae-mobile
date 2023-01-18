3e36778b4bf5f7ad10309274b2fb0d16b6070901
SeongMin <2018320221@korea.ac.kr>
2023-01-18 18:22:46 +0900

    modified:   lib/api/account_api.dart 	modified:   lib/api/auth_api.dart 	modified:   lib/api/habit_api.dart

--- lib/api/account_api.dart
+++ lib/api/account_api.dart
@@ -18,6 +18,19 @@ class AccountApi {
   }
 
   // 사용자의 이름, 사진을 수정하는 메커니즘을 정의한다.
+  Future<int> updateProfile(String name, String photo) async {
+    final response = await _dio.patch<Map<String, dynamic>>(
+      '/users/profile',
+      data: {'name': name, 'photo': photo},
+    );
+    final json = response.statusCode!;
+    return json;
+  }
 
   // 사용자 계정 삭제 메커니즘을 정의한다.
+  Future<int> deleteProfile() async {
+    final response = await _dio.delete<Map<String, dynamic>>('/users');
+    final json = response.statusCode!;
+    return json;
+  }
 }
